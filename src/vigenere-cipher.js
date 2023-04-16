2af909396ce6c3d0ce9b8f577381a12c86687fa7
Denis Gusser <gusser.denis@gmail.com>
2023-04-17 01:13:05 +0400

    task: complete cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let encryptedMessage = '',
+        j = 0;
+
+    for (let symbol of message) {
+      const keySymbol = key[j % key.length];
+
+      if (alphabet.includes(symbol)) {
+
+        let encryptedSymbolIndex = ((alphabet.indexOf(symbol) + alphabet.indexOf(keySymbol)) % 26),
+            encryptedSymbol = alphabet[encryptedSymbolIndex];
+
+        encryptedMessage += encryptedSymbol;
+        j++;
+
+      } else {
+        encryptedMessage += symbol;
+      }
+    }
+
+    return this.direct ? encryptedMessage : encryptedMessage.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let message = '',
+        j = 0;
+
+    for (let symbol of encryptedMessage) {
+      const keySymbol = key[j % key.length];
+
+      if (alphabet.includes(symbol)) {
+
+        let messageSymbolIndex = (((alphabet.indexOf(symbol) - alphabet.indexOf(keySymbol)) % 26) + 26) % 26,
+            messageSymbol = alphabet[messageSymbolIndex];
+
+        message += messageSymbol;
+        j++;
+
+      } else {
+        message += symbol;
+      }
+    }
+
+    return this.direct ? message : message.split('').reverse().join('');
   }
 }
 
