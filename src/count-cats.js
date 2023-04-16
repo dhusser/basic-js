1b16dd29ace4db46801fd51b67a095b7fa744a7a
Denis Gusser <gusser.denis@gmail.com>
2023-04-16 23:01:13 +0400

    refactor: refactor count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,17 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(matrix) {
-  // throw new NotImplementedError('Not implemented');
-  let count = 0;
-  for(let i in matrix) {
-    for(let j in matrix[i]) {
-      if(matrix[i][j] === '^^') count++;
-    }
-  }
 
-  return count
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let count = 0;
+  matrix.forEach(row => {
+    row.forEach(cell => {
+      if(cell === '^^') count++;
+    })
+  })
+
+  return count;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
+function getSeason(date) {
   throw new NotImplementedError('Not implemented');
+//   if (date instanceof Date == false) throw new Error("Invalid date!");
+//   if (date) {
+
+//     switch (date.getMonth()) {
+//       case 0:
+//     case 1:
+//       return 'winter'
+//       break;
+//     case 2:
+//     case 3:
+//     case 4:
+//       return 'spring'
+//       break;
+//     case 5:
+//     case 6:
+//     case 7:
+//       return 'summer'
+//       break;
+//     case 8:
+//     case 9:
+//     case 10:
+//       return 'autumn'
+//       break;
+//     case 11:
+//       return 'winter'
+//       break;
+//   }
+// } else {
+//   return 'Unable to determine the time of year!'
+// }
   // remove line with error and write your code here
 }
 
