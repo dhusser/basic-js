4058ff05d057500d67dfbaa43d73d33ccf309d57
Denis Gusser <gusser.denis@gmail.com>
2023-04-17 00:09:29 +0400

    task: complete hanoi towers

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  const numberOfTurns = Math.pow(2, disksNumber) - 1,
+        time = Math.floor((numberOfTurns / turnsSpeed) * 3600);
+
+        return {"turns": numberOfTurns, "seconds": time}
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,6 +11,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
 */
+
 function getSeason(date) {
   if (!date) {
     return 'Unable to determine the time of year!';
